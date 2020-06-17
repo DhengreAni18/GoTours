8b9a969839c49bb0b9d054466eeed10ec09ae81c
Ani Dhengre <dhengreani18@gmail.com>
2020-06-17 20:33:22 +0530

    Added MongoDB Connection

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv");
 const app = require("./app");
-const mongoose = require("./mongoose");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD);
 
-mongoose.connect();
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("DB Connected");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
