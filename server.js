36c8f2ff6e870a9c6eae4638622ef27d3daa3542
Ani Dhengre <dhengreani18@gmail.com>
2020-06-20 21:52:20 +0530

    Added Tour model and schema

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ mongoose
     console.log("DB Connected");
   });
 
+const TourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, "A Tour must have a name"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A Tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", TourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
