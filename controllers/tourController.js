936bf188173bacab04974a12fb3203834c818ad1
Ani Dhengre <dhengreani18@gmail.com>
2020-06-23 21:50:37 +0530

    Add method to find tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,18 +40,24 @@ exports.getAllTours = async (req, res) => {
   });
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      message: err,
+    });
+  }
 
   // const tour = tours.find(el => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour
-  //   }
-  // });
 };
 
 exports.createTour = async (req, res) => {
